eb441df6ee2522359fa09e8ff7bdded2cea297bd
Multezem Kedir <kedirmultezem@gmail.com>
2019-03-07 08:40:08 +0000

    load and insert

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from xml.etree import ElementTree
+import json
 
 app = Flask(__name__)
 
@@ -11,36 +12,61 @@ root = tree.getroot()
 #creat new xml data
 def newXMLElem(title, des, url):
     news_element = ElementTree.SubElement(root, "news")
-    news_element.set('img',url)
+    news_element.set('image',url)
     headline_element = ElementTree.SubElement(news_element, "headline")
     headline_element.text = title
     description__element= ElementTree.SubElement(news_element, "description")
     description__element.text = des
+    tree.write(path, "UTF-8")
+    print("Done writing")
 
 
 #get xml data
-#get list of image urls
-image_url = [c.attrib["image"] for c in root.iter('news')]
-#get list of headlines
-headers = [c.find('headline').text for c in root.iter('news')]
-#get list of description
-descriptions = [c.find('description').text for c in root.iter('news')]
+def loadData():
+    #get list of image urls
+    image_url = [c.attrib["image"] for c in root.iter('news')]
+    #get list of headlines
+    headers = [c.find('headline').text for c in root.iter('news')]
+    #get list of description
+    descriptions = [c.find('description').text for c in root.iter('news')]
 
-#organize data into dictionary
-data_dictionary = {"url":image_url,"header":headers,"desc":descriptions}
+    #organize data into dictionary
+    data_dictionary = {"url":image_url,"header":headers,"desc":descriptions}
 
-#size of data
-size = len(d["header"])
+    #size of data
+    size = len(data_dictionary["header"])
+
+    return data_dictionary, size
 
 @app.route('/')
 def home():
-    return render_template('index.html', data=data_dictionary, length=size)
+    data_in, s = loadData()
+
+    return render_template('index.html', data=data_in, length=s)
 
 @app.route('/insert', methods=['GET', 'POST'])
 def insert_data():
+    #this block is only entered when the form is submitted
+    if request.method == 'POST':
+        req_data = request.get_data();
+        print(" *"*50, end="")
+        print("Posted data", end="")
+        print(" *"*50)
+        print(req_data)
+        print(" *"*100)
+        head = request.form['headline']
+        des = request.form['des']
+        image = request.form['url']
+        newXMLElem(head, des,image)
+        return '''<h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(head,des, image)
+
+
     return '''<form method="POST">
-                  Language: <input type="text" name="language"><br>
-                  Framework: <input type="text" name="framework"><br>
+                  Headline: <input type="text" name="headline"><br>
+                  Image: <input type="url" name="url"><br>
+                  Description: <input type="text" name="des"><br>
                   <input type="submit" value="Submit"><br>
               </form>
           '''
@@ -49,4 +75,4 @@ def insert_data():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
